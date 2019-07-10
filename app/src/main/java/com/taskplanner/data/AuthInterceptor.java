cf223afc710fe8555e295fc13a1145380044c15f
SergeiZinkovich <my_agent-1@mail.ru>
2019-07-11 01:08:01 +1000

    Now use user token in interceptor

--- app/src/main/java/com/taskplanner/data/AuthInterceptor.java
+++ app/src/main/java/com/taskplanner/data/AuthInterceptor.java
@@ -1,5 +1,7 @@
 package com.taskplanner.data;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.io.IOException;
 
 import okhttp3.Interceptor;
@@ -10,9 +12,10 @@ public class AuthInterceptor implements Interceptor {
 
     @Override
     public Response intercept(Chain chain) throws IOException {
+        String tokenId = FirebaseAuth.getInstance().getCurrentUser().getIdToken(false).getResult().getToken();
         Request original = chain.request();
         Request request = original.newBuilder()
-                .header("X-Firebase-Auth", "serega_mem")
+                .header("X-Firebase-Auth", tokenId != null ? tokenId : "")
                 .method(original.method(), original.body())
                 .build();
 
