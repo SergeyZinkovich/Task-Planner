df6a538a1e9d58cc0b8008a4700598585eacb6bd
SergeiZinkovich <my_agent-1@mail.ru>
2019-07-11 23:47:46 +1000

    Date engine code refactor

--- app/src/main/java/com/taskplanner/DataEngine.java
+++ app/src/main/java/com/taskplanner/DataEngine.java
@@ -16,11 +16,11 @@ import java.util.Calendar;
 public class DataEngine {
 
     public interface GetEventCallback{
-        public void setEvents(Calendar calendar, ArrayList<EventModel> events);
+        void setEvents(Calendar calendar, ArrayList<EventModel> events);
     }
 
     public interface RequestEventCallback {
-        public void requestEventSuccess(boolean success);
+        void requestEventSuccess(boolean success);
     }
 
     private EventRepository eventRepository;
@@ -40,14 +40,13 @@ public class DataEngine {
     public void getEventModels(Calendar from, Calendar to, GetEventCallback getEventCallback){
         Long f = from.getTimeInMillis();
         Long t = to.getTimeInMillis();
-        eventRepository.getEventsByInterval(f, t).subscribe(ans -> {
-            setEvents(ans, from, getEventCallback);
-        }, throwable -> {
-            Log.e("Network error in get event:", throwable.getMessage());
-        });
+        eventRepository.getEventsByInterval(f, t).subscribe(
+                response -> getPatterns(convertEntityToEventModels(response), from, getEventCallback),
+                throwable -> Log.e("Network error in get event:", throwable.getMessage())
+        );
     }
 
-    private void setEvents(EventResponseEntity eventResponseEntity, Calendar calendar, GetEventCallback getEventCallback){
+    private ArrayMap<Long, EventModel> convertEntityToEventModels(EventResponseEntity eventResponseEntity){
         ArrayMap<Long, EventModel> events = new ArrayMap<>();
         for (EventEntity eventEntity: eventResponseEntity.getData()){
             EventModel event = new EventModel();
@@ -58,27 +57,26 @@ public class DataEngine {
             event.setStatus(eventEntity.getStatus());
             events.put(event.getId(), event);
         }
-        if(events.size() != 0){
-            getPatterns(events, calendar, getEventCallback);
-        }
+        return events;
     }
 
     private void getPatterns(ArrayMap<Long, EventModel> events, Calendar calendar, GetEventCallback getEventCallback){
+        if (events.size() ==0){
+            return;
+        }
         Long[] ids = new Long[events.size()];
         int i = 0;
         for (EventModel event: events.values()){
             ids[i] = event.getId();
             i++;
         }
-        eventPatternRepository.getPatternsByEventsIds(ids).subscribe(ans -> {
-            setPatterns(ans, events, calendar, getEventCallback);
-        }, throwable -> {
-            Log.e("Network error in get pattern:", throwable.getMessage());
-        });
+        eventPatternRepository.getPatternsByEventsIds(ids).subscribe(
+                response -> getEventCallback.setEvents(calendar, addPatternsToEventModels(response, events)),
+                throwable -> Log.e("Network error in get pattern:", throwable.getMessage())
+        );
     }
 
-    private void setPatterns(EventPatternsResponseEntity response,ArrayMap<Long, EventModel> events,  //TODO: мб вынести колбек
-                             Calendar calendar, GetEventCallback getEventCallback){
+    private ArrayList<EventModel> addPatternsToEventModels(EventPatternsResponseEntity response, ArrayMap<Long, EventModel> events){
         for (EventPatternEntity pattern: response.getData()){
             EventModel ev = events.get(pattern.getEventId());
             ev.setPatternId(pattern.getId());
@@ -86,24 +84,23 @@ public class DataEngine {
             ev.setEndTimeFromDuration(pattern.getDuration());
             ev.setRrule(pattern.getRrule());
         }
-        getEventCallback.setEvents(calendar, new ArrayList<EventModel>(events.values()));
+        return new ArrayList<>(events.values());
     }
 
     public void deleteEvent(EventModel event, RequestEventCallback requestEventCallback){
-        eventRepository.deleteEvent(event.getId()).subscribe(ans -> {
-            requestEventCallback.requestEventSuccess(ans.isSuccess());
-        }, throwable -> {
-            Log.e("Network error in delete event:", throwable.getMessage());
-        });
+        eventRepository.deleteEvent(event.getId()).subscribe(
+                response -> requestEventCallback.requestEventSuccess(response.isSuccess()),
+                throwable -> Log.e("Network error in delete event:", throwable.getMessage())
+        );
     }
 
     public void saveEvent(EventModel event, RequestEventCallback requestEventCallback){
-        eventRepository.saveEvent(convertEventModelToEntity(event)).subscribe(ans -> {
-            savePatterns(ans.getData()[0].getId(), event, requestEventCallback);
-        }, throwable -> {
-            Log.e("Network error in save event:", throwable.getMessage());
-            requestEventCallback.requestEventSuccess(false);
-        });
+        eventRepository.saveEvent(convertEventModelToEntity(event)).subscribe(
+                response -> savePatterns(response.getData()[0].getId(), event, requestEventCallback),
+                throwable -> {
+                    Log.e("Network error in save event:", throwable.getMessage());
+                    requestEventCallback.requestEventSuccess(false);
+                });
     }
 
     private EventEntity convertEventModelToEntity(EventModel eventModel){
@@ -114,12 +111,12 @@ public class DataEngine {
     }
 
     private void savePatterns(Long eventId, EventModel event, RequestEventCallback requestEventCallback){
-        eventPatternRepository.savePattern(eventId, convertEventModelToPattern(event)).subscribe(ans -> {
-            requestEventCallback.requestEventSuccess(true);
-        }, throwable -> {
-            Log.e("Network error in save pattern:", throwable.getMessage());
-            requestEventCallback.requestEventSuccess(false);
-        });
+        eventPatternRepository.savePattern(eventId, convertEventModelToPattern(event)).subscribe(
+                response -> requestEventCallback.requestEventSuccess(true),
+                throwable -> {
+                    Log.e("Network error in save pattern:", throwable.getMessage());
+                    requestEventCallback.requestEventSuccess(false);
+                });
     }
 
     private EventPatternEntity convertEventModelToPattern(EventModel event){
@@ -131,20 +128,20 @@ public class DataEngine {
     }
 
     public void updateEvent(EventModel event, RequestEventCallback requestEventCallback){
-        eventRepository.updateEvent(event.getId(), convertEventModelToEntity(event)).subscribe(ans -> {
-            updatePattern(event, requestEventCallback);
-        }, throwable -> {
-            Log.e("Network error in update event:", throwable.getMessage());
-            requestEventCallback.requestEventSuccess(false);
-        });
+        eventRepository.updateEvent(event.getId(), convertEventModelToEntity(event)).subscribe(
+                response -> updatePattern(event, requestEventCallback),
+                throwable -> {
+                    Log.e("Network error in update event:", throwable.getMessage());
+                    requestEventCallback.requestEventSuccess(false);
+                });
     }
 
     private void updatePattern(EventModel event, RequestEventCallback requestEventCallback){
-        eventPatternRepository.updatePattern(event.getPatternId(), convertEventModelToPattern(event)).subscribe(ans -> {
-            requestEventCallback.requestEventSuccess(true);
-        }, throwable -> {
-            Log.e("Network error in update pattern:", throwable.getMessage());
-            requestEventCallback.requestEventSuccess(false);
-        });
+        eventPatternRepository.updatePattern(event.getPatternId(), convertEventModelToPattern(event)).subscribe(
+                response -> requestEventCallback.requestEventSuccess(true),
+                throwable -> {
+                    Log.e("Network error in update pattern:", throwable.getMessage());
+                    requestEventCallback.requestEventSuccess(false);
+                });
     }
 }
