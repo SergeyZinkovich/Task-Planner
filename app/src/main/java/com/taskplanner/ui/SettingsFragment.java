7ede58e10333524401de462705dfa5a890c15a9c
SergeiZinkovich <my_agent-1@mail.ru>
2019-07-16 14:37:25 +1000

    Added user info and ability to login/logout in settings screen

--- app/src/main/java/com/taskplanner/ui/SettingsFragment.java
+++ app/src/main/java/com/taskplanner/ui/SettingsFragment.java
@@ -7,10 +7,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.arellomobile.mvp.MvpAppCompatFragment;
@@ -20,32 +23,42 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 import com.taskplanner.App;
 import com.taskplanner.R;
 import com.taskplanner.Screens;
 import com.taskplanner.presenter.SettingsFragmentPresenter;
 
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import ru.terrakok.cicerone.Router;
 
 public class SettingsFragment extends MvpAppCompatFragment implements SettingsFragmentView {
 
-    private FirebaseUser user;
-
-    private static int RC_SIGN_IN = 9001; //Todo: вынести
+    @BindView(R.id.ivUserAvatar)
+    ImageView ivUserAvatar;
+    @BindView(R.id.tvName)
+    TextView tvName;
+    @BindView(R.id.tvEmail)
+    TextView tvEmail;
+    @BindView(R.id.authButton)
+    Button authButton;
+    @BindView(R.id.cvUserAvatar)
+    CardView cvUserAvatar;
 
     @InjectPresenter
     SettingsFragmentPresenter settingsFragmentPresenter;
 
     @Inject
     Router router;
+
+    private static int RC_SIGN_IN = 9001; //Todo: вынести
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,22 +76,49 @@ public class SettingsFragment extends MvpAppCompatFragment implements SettingsFr
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings_fragment, container, false);
         ButterKnife.bind(this, view);
-
+        setAuthViews();
         return view;
     }
 
-    @OnClick(R.id.logoutButton)
-    public void onLogoutButtonClick(TextView textView){
-        AuthUI.getInstance()
-                .signOut(getContext())
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    public void onComplete(@NonNull Task<Void> task) {
-                        LogIn();
-                    }
-                });
+    private void setAuthViews(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            tvEmail.setVisibility(View.VISIBLE);
+            tvName.setVisibility(View.VISIBLE);
+            cvUserAvatar.setVisibility(View.VISIBLE);
+            tvName.setText(user.getDisplayName());
+            tvEmail.setText(user.getEmail());
+            Picasso.with(getContext())
+                    .load(user.getPhotoUrl())
+                    .placeholder(R.mipmap.ic_launcher_round)
+                    .into(ivUserAvatar);
+            authButton.setText(R.string.logout_button_text);
+        }
+        else{
+            tvEmail.setVisibility(View.GONE);
+            tvName.setVisibility(View.GONE);
+            cvUserAvatar.setVisibility(View.GONE);
+            authButton.setText(R.string.login_button_text);
+        }
     }
 
-    public void LogIn(){
+    @OnClick(R.id.authButton)
+    public void onLogoutButtonClick(TextView textView){
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            AuthUI.getInstance()
+                    .signOut(getContext())
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            setAuthViews();
+                        }
+                    });
+        }
+        else {
+            LogIn();
+        }
+    }
+
+    private void LogIn(){
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
                 new AuthUI.IdpConfig.GoogleBuilder().build());
@@ -96,12 +136,7 @@ public class SettingsFragment extends MvpAppCompatFragment implements SettingsFr
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
-            if (resultCode == Activity.RESULT_OK) {
-                user = FirebaseAuth.getInstance().getCurrentUser();
-                router.newRootScreen(Screens.SCREEN_MONTH_FRAGMENT);
-            } else {
-                getActivity().finish();
-            }
+            setAuthViews();
         }
     }
 
